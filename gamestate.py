41e836273aa14a3508bd46576ebaeef006818e61
Lenn Messelken <lenn.messelken@gmail.com>
2025-06-19 19:46:28 +0200

    Added Last Move and Valid Move Highlighting and implemented Basic Valid Moves for Pawns including Promotion

--- gamestate.py
+++ gamestate.py
@@ -11,23 +11,118 @@ class Gamestate:
             ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']]
         
         self.white_turn = True
+        self.colors_turn = 'w' if self.white_turn else 'b'
         self.checkmate = False
         self.stalemate = False
 
+        self.move_log = []
+
 
     def make_move(self, move):
-        self.board[move.start_column][move.start_row] = "--"
-        self.board[move.end_column][move.end_row] = move.piece_moving
+        self.board[move.start_row][move.start_column] = "--"
+        if move.promotion == True:
+            valid_choices = {'N', 'B', 'R', 'Q'}
+            choice = ""
+            while True:
+                user_input = input("Please input which piece you want to Promote to: (N) Knight (B) Bishop (R) Rook (Q) Queen:\n").upper()
+                if user_input in valid_choices:
+                    choice = user_input
+                    break
+            move.piece_moving = move.piece_moving[0] + choice
+        self.board[move.end_row][move.end_column] = move.piece_moving
+        self.move_log.append(move)
+        self.white_turn = not self.white_turn
+        self.colors_turn = 'w' if self.white_turn else 'b'
 
     def get_valid_moves(self):
-        pass
+        moves = []
+        for row in range(8):
+            for col in range(8):
+                if self.board[row][col][0] != self.colors_turn:
+                    continue
+                moves.extend(self.get_piece_moves(row, col))
+               
+        
+        #for i in range(len(moves)):
+        #   print(f"({moves[i].start_row, moves[i].start_column }) => ({moves[i].end_row, moves[i].end_column }) Promotion: {moves[i].promotion}")
 
+        return moves
+
+    def get_piece_moves(self, row, col):
+        match self.board[row][col][1]:
+                case 'P':
+                    moves = self.get_valid_pawn_moves(row, col)
+                case 'N':
+                    moves = self.get_valid_knight_moves(row, col)
+                case 'B':
+                    moves = self.get_valid_bishop_moves(row, col)
+                case 'R':
+                    moves = self.get_valid_rook_moves(row, col)
+                case 'Q':
+                    moves = self.get_valid_queen_moves(row, col)
+                case 'K':
+                    moves = self.get_valid_king_moves(row, col)
+        return moves
+
+    def get_valid_pawn_moves(self, row, col):
+        moves = []
+        if self.colors_turn == 'w':
+            move_amount = -1
+            home_row = 6
+            back_row = 0
+            opponent_color = 'b'
+            
+        else:
+            move_amount = 1
+            home_row = 1
+            back_row = 7
+            opponent_color = 'w'
+            
+        promotion = False
+
+        if self.board[row + move_amount][col] ==  "--":
+            if row + move_amount == back_row:
+                promotion = True
+            moves.append(Move((row, col), (row + move_amount, col), self.board, promotion))
+
+        if row == home_row and self.board[row + 2 * move_amount][col] ==  "--":
+            moves.append(Move((row, col), (row + 2* move_amount, col), self.board, promotion))
+        if col-1 >= -1:
+            if self.board[row + move_amount][col-1][0] == opponent_color:
+                if row + move_amount == back_row:
+                    promotion = True
+                moves.append(Move((row, col), (row + move_amount, col-1), self.board, promotion))
+        if col+1 < 8:
+            if self.board[row + move_amount][col+1][0] == opponent_color:
+                if row + move_amount == back_row:
+                    promotion = True
+                moves.append(Move((row, col), (row + move_amount, col+1), self.board, promotion))
+        
+        return moves
+            
+
+    def get_valid_knight_moves(self, row, col):
+        return []
+    def get_valid_bishop_moves(self, row, col):
+        return []
+    def get_valid_rook_moves(self, row, col):
+        return []
+    def get_valid_queen_moves(self, row, col):
+        return []
+    def get_valid_king_moves(self, row, col):
+        return []
 
 
 class Move:
-    def __init__(self, start_square, end_square, board):
+    def __init__(self, start_square, end_square, board, promotion = False):
         self.start_row, self.start_column = start_square
         self.end_row, self.end_column = end_square
-        self.piece_moving = board[self.start_column][self.start_row]
+        self.piece_moving = board[self.start_row][self.start_column]
+        self.promotion = promotion
+        self.unique_id = self.start_row * 1000 + self.start_column * 100 + self.end_row * 10 + self.end_column
         
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.unique_id == other.unique_id
+        return False
         
--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def load_images(): #fills images dictionary with correct files
 load_images()
 
 def main():
+    
 
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     clock = pygame.time.Clock()
@@ -31,6 +32,7 @@ def main():
 
     #global varialbes config
     gamestate = Gamestate() #gamestate Object
+    gamestate.get_valid_moves()
     square_selected = () #keeps track of last player click (single tuple)
     player_clicks = [] #keeps track of players clicks (up to two tuples)
     move_made = False
@@ -50,23 +52,32 @@ def main():
                 column = location[0] // SQUARE_SIZE
                 row = location[1] // SQUARE_SIZE
 
-                if square_selected == (column, row): #player clicks on same square 
+                if square_selected == (row, column): #player clicks on same square 
                     square_selected = () # Deselects square
                     player_clicks = []
 
                 else: #player clicks on new square
-                    square_selected = (column, row)
-                    player_clicks.append(square_selected)
+                    if len(player_clicks) == 0: #first piece has to be of turn player
+                         #which colors turn
+                        if gamestate.board[row][column][0] == gamestate.colors_turn: #player clicks on own piece
+                            square_selected = (row, column)
+                            player_clicks.append(square_selected)
+                    else:
+                        square_selected = (row, column)
+                        player_clicks.append(square_selected)
 
                 if len(player_clicks) == 2: #player has clicked on two different squares
                     move = Move(player_clicks[0], player_clicks[1], gamestate.board)
                     valid_moves = gamestate.get_valid_moves()
-                    if True:
                     #for i in range(len(valid_moves)):
-                        #if move == valid_moves[i]: #move is valid
-                        if True:
-                            #gamestate.make_move(valid_moves[i])
-                            gamestate.make_move(move)
+                    #    print(f"valid  ({valid_moves[i].start_row, valid_moves[i].start_column }) => ({valid_moves[i].end_row, valid_moves[i].end_column }) Promotion: {valid_moves[i].promotion}")
+
+                    #print(f"actual ({move.start_row, move.start_column }) => ({move.end_row, move.end_column }) Promotion: {move.promotion}")
+                    for i in range(len(valid_moves)):
+                        #print(move, valid_moves[i])
+                        if move == valid_moves[i]: #move is valid
+                            gamestate.make_move(valid_moves[i])
+                            #gamestate.make_move(move)
                             move_made = True
                             square_selected = () #clean up
                             player_clicks = [] #clean up
@@ -76,6 +87,7 @@ def main():
 
                     if move_made: #resets
                         move_made = False
+                        
 
         draw_gamestate(screen, gamestate, square_selected)
         clock.tick(MAX_FPS)
@@ -97,15 +109,31 @@ def draw_board(screen):
             pygame.draw.rect(screen, color, (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
 def highlight_squares(screen, gamestate, square_selected):
+    if len(gamestate.move_log) != 0:
+        last_move = gamestate.move_log[-1]
+        start_row, start_column = last_move.start_row, last_move.start_column
+        end_row, end_column = last_move.end_row, last_move.end_column
+        s = pygame.Surface((SQUARE_SIZE, SQUARE_SIZE))
+        #s.set_alpha(100)
+        s.fill(pygame.Color((255, 255, 0)))
+        screen.blit(s, (start_column * SQUARE_SIZE, start_row * SQUARE_SIZE))
+        screen.blit(s, (end_column * SQUARE_SIZE, end_row * SQUARE_SIZE))
+
     if square_selected != ():
         row, column = square_selected
-        colors_turn = 'w' if gamestate.white_turn else 'b' #which colors turn
-        if gamestate.board[column][row][0] == colors_turn: #player clicks on own piece
+        
+        if gamestate.board[row][column][0] == gamestate.colors_turn: #player clicks on own piece
             
             s = pygame.Surface((SQUARE_SIZE, SQUARE_SIZE))
-            s.set_alpha(100) #color should be transparent so color underneath can be seen
+            #s.set_alpha(100) #color should be transparent so color underneath can be seen
             s.fill(pygame.Color((0,255,0)))
-            screen.blit(s, (row* SQUARE_SIZE, column * SQUARE_SIZE))
+            screen.blit(s, (column* SQUARE_SIZE, row * SQUARE_SIZE))
+            s.fill(pygame.Color((0,0,255)))
+            moves = []
+            moves = gamestate.get_piece_moves(row, column)
+            for i in range(len(moves)):
+                screen.blit(s, (moves[i].end_column * SQUARE_SIZE, moves[i].end_row * SQUARE_SIZE))
+    
 
 
 def draw_pieces(screen, gamestate):
